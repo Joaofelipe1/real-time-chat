66c6607dfc9b3d9a5f06a9e7c505e708b3176391
Joaofelipe1 <joaofelipeveiga30@gmail.com>
2025-01-28 11:02:04 -0300

    learning new concepts

--- server.js
+++ server.js
@@ -1,23 +1,105 @@
 import {ApolloServer, gql} from 'apollo-server'
+import { error } from 'console'
+import {randomUUID} from 'crypto'
+import { title } from 'process'
 
+const users = [
+    { 
+        id: 1,
+        firstName: "joao",
+        lastName: "felipe",
+        email: "felipe@gmail.com",
+        password: "12345"
+    },
+    {
+        id: 2,
+        firstName: "daniel",
+        lastName: "robert",
+        email: "robert@gmail.com",
+        password: "34656"
+    }
+]
+
+const Todos = [
+    {
+        title:"buy book",
+        by: 1
+    },
+    {
+        title:"write code",
+        by: 1 
+    },
+    {
+        title:"record video",
+        by: 2
+    }
+]
 
 const typeDefs = gql`
     type Query{
-        greet:String
+        users:[User]
+        user(id:ID!):User
+    }
+
+    input UserInput{
+        firstName:String!
+        lastName:String!
+        email:String!
+        password:String!
+    }
+
+    type Mutation{
+        createUser(userNew:UserInput!):User
+    }
+
+    type User{
+        id:ID!
+        firstName:String!
+        lastName:String!
+        email:String!
+        todos:[Todo]
+    }
+
+    type Todo {
+        title:String!
+        by:ID!
     }
 `
 
 const resolvers = {
     Query:{
-        greet:()=>{
-            return "Hello world"
+        users:()=>users,
+        user:(_, {id}, {userLoggedIn})=>{
+            if(!userLoggedIn) throw new Error("You are not logged in")
+            return users.find(item=>item.id == id)    
+        }
+    },
+    User:{
+        todos:(parent)=>{
+            return Todos.filter(todo=>todo.by == parent.id)
+        }
+    },
+    Mutation:{
+        createUser:(_,{userNew})=>{
+        const newUser = {
+            id:crypto.randomUUID(),
+                ...userNew
+        }
+            users.push(newUser)
+            return newUser;
         }
     }
 }
 
 
 
-const server = new ApolloServer({ typeDefs, resolvers});
+const server = new ApolloServer({ 
+    typeDefs,
+    resolvers,
+    context:{
+        userLoggedIn: true
+    }
+});
 
 server.listen().then(({ url })=> {
     console.log( `Server ready at ${url}`);
